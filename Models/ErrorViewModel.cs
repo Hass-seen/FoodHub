42c909a9f11378474cf552a2b88644f44ac6a456
Hass <seennd1@gmail.com>
2022-06-25 13:30:01 +0300

    gitting started

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodHub.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Ingreadiants { get; set; }
+        public string Discription { get; set; }
+
+        public DateTime DateOfCreation { get; set; } = DateTime.Now;
+
+    }
+}
