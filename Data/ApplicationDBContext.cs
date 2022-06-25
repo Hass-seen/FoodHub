7807ac2af29db318503ce230131fac819394c6c0
Hass <seennd1@gmail.com>
2022-06-25 22:01:07 +0300

    added DB connection

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using FoodHub.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDBContext : DbContext 
+{
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) :base(options)
+    {
+        
+    }
+
+    public DbSet<Recipe> Recipes {get; set;}
+}
