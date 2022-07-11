db4b50912707bed4c8d0dca92fec12b3e3512eaa
Hass <seennd1@gmail.com>
2022-07-11 19:52:09 +0300

    full CRUD on ingreadiant

--- Controllers/IngreadiantController.cs
+++ Controllers/IngreadiantController.cs
@@ -37,9 +37,60 @@ public class IngreadiantController : Controller
             folder += Guid.NewGuid().ToString()+(obj.Image.FileName) ;
             string serverfolder= Path.Combine( _IWebHostEnvironment.WebRootPath,folder);
            await obj.Image.CopyToAsync(new FileStream(serverfolder,FileMode.Create));
-           obj.path= serverfolder;
+           
+           obj.path="/"+ folder;
+        }else{
+            obj.path="/images/default.jpg";
         }
         _db.Ingreadiants.Add(obj);
+        _db.SaveChanges();
+
+        return RedirectToAction("IndexIng");
+    }
+
+
+
+         public IActionResult Edite(String name)
+    {   
+        
+        var ing =_db.Ingreadiants.Find(name);
+        return View(ing);
+    }
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edite(Ingreadiant obj)
+    {
+        if(obj.Image!=null){
+            string folder= "Images/";
+            folder += Guid.NewGuid().ToString()+(obj.Image.FileName) ;
+            string serverfolder= Path.Combine( _IWebHostEnvironment.WebRootPath,folder);
+           await obj.Image.CopyToAsync(new FileStream(serverfolder,FileMode.Create));
+           
+           obj.path="/"+ folder;
+        }
+        _db.Ingreadiants.Update(obj);
+        _db.SaveChanges();
+
+        return RedirectToAction("IndexIng");
+    }
+
+
+         public IActionResult Delete(String name)
+    {   
+        
+        var ing =_db.Ingreadiants.Find(name);
+        return View(ing);
+    }
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(Ingreadiant obj)
+    {
+        _db.Ingreadiants.Remove(obj);
+        _db.SaveChanges();
 
         return RedirectToAction("IndexIng");
     }
---
+++ Migrations/20220711160746_addingImagePath.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodHub.Migrations
+{
+    public partial class addingImagePath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "path",
+                table: "Ingreadiants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "path",
+                table: "Ingreadiants");
+        }
+    }
+}
